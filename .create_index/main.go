63b08f780b657c2c0ffdbb30412b4dff96a914df
sters <sters@users.noreply.github.com>
2021-01-16 15:25:11 +0900

    update

---
+++ .create_index/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+const tpl = `
+<ul>
+{{ range $key, $value := . }}
+<li><a href="/{{ $key }}/">{{ $value }}</a></li>
+{{ end }}
+</ul>
+`
+
+func main() {
+	dirs := map[string]string{
+		"video_test": "MediaStreamを使ったWebcam表示テスト",
+	}
+
+	_, file, _, _ := runtime.Caller(0)
+	root := filepath.Dir(filepath.Dir(file))
+	fmt.Println(root)
+
+	fileBuf, err := os.Create(filepath.Join(root, "index.html"))
+	if err != nil {
+		panic(err)
+	}
+	defer fileBuf.Close()
+
+	t, err := template.New("").Parse(tpl)
+	if err != nil {
+		panic(err)
+	}
+	if err := t.Execute(fileBuf, dirs); err != nil {
+		panic(err)
+	}
+}
--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@
 .PHONY: serve
 serve:
 	php -S localhost:8000
+
+.PHONY: index
+index:
+	go run .create_index/main.go
