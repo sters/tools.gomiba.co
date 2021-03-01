5be322a44c2bcb0cad3c904c04dc859da103bb87
sters <sters@users.noreply.github.com>
2021-03-02 02:53:37 +0900

    remove unused codes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,21 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import Vue from 'vue';
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+export default Vue.extend({
+  name: 'HelloWorld',
+
+  data: () => ({}),
+});
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import ja from 'vuetify/src/locale/ja';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  lang: {
+    locales: { ja },
+    current: 'ja',
+  },
+});
